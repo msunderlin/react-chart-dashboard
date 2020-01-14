1ed504a040dd9e4f5f5186082e4a10a63061d272
Mike Sunderlin <msunderlin@gmail.com>
2020-01-13 22:22:34 -0800

    Updates

---
+++ precache-manifest.ff27661cfa55792211c797679b5d0538.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3466a7b26c110166485ed19da227eab9",
+    "url": "/react-chart-dashboard/index.html"
+  },
+  {
+    "revision": "57f5534dca83c945706b",
+    "url": "/react-chart-dashboard/static/js/2.7983ac13.chunk.js"
+  },
+  {
+    "revision": "cd3a6ddfdc6688027c943bf710eff50b",
+    "url": "/react-chart-dashboard/static/js/2.7983ac13.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a156935fefd8c10bb2d",
+    "url": "/react-chart-dashboard/static/js/main.ccd878a9.chunk.js"
+  },
+  {
+    "revision": "b3ea8692679729bc4468",
+    "url": "/react-chart-dashboard/static/js/runtime-main.79fac8d6.js"
+  }
+]);
