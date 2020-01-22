ccf830eae7f185b36e25331cc983d870996036bc
Mike Sunderlin <msunderlin@gmail.com>
2020-01-21 22:47:55 -0800

    working through issues with the chartwrapper

---
+++ src/components/charts/ChartWrapper.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import BarChart from './BarChart'; 
+import LineChart from './LineChart'; 
+import DoughnutChart from './DoughnutChart'; 
+import PieChart from './PieChart'; 
+import PolarChart from './PolarChart'; 
+
+
+function ChartWrapper (props){
+	const charts = props.charts;
+
+	for(let i =0;i<charts.length() i++){
+
+	}
+}
+export default ChartWrapper;
