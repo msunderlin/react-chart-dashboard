927188c5cf7d6bce55e6c9e60bce6d159a30f9c5
Michael Sunderlin <msunderlin@gmail.com>
2020-01-28 18:58:54 -0800

    working though the table component

--- src/components/table/StickyHeadTable.js
+++ src/components/table/StickyHeadTable.js
@@ -1,168 +1,34 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import {
-  TableContainer,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-  TablePagination
-} from "@material-ui/core";
 
-const useStyles = makeStyles({
-  root: {
-    width: "100%"
-  },
-  container: {
-    maxHeight: 200
+import MaterialTable from "material-table";
+
+
+
+
+class Table extends React.Component{
+    constructor(props){
+      super(props);
+      fetch(this.props.chart.source) 
+      .then(response => response.json())
+      .then(data=>{
+          this.setState({data:data});
+      });
+    }
+    componentDidMount() {
+      window.setInterval(() => {
+          fetch(this.props.chart.source)
+          .then(response => response.json())
+          .then(data=>{
+              this.setState({data:data});
+          });
+      }, this.props.chart.interval);
+    }
+  render(){
+    return (
+      <TableMaterialTable   
+        title={this.props.}
+      />
+    );
   }
-});
 
-function StickyHeadTable(props) {
-  const classes = useStyles();
-  console.log(props);
-  return (
-    <>
-      <h3 className="title">{props.title}</h3>
-      <TableContainer className={classes.container}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              <TableCell>0</TableCell>
-              <TableCell>1</TableCell>
-              <TableCell>2</TableCell>
-              <TableCell>3</TableCell>
-              <TableCell>4</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <TableRow>
-              <TableCell>0</TableCell>
-              <TableCell>1</TableCell>
-              <TableCell>2</TableCell>
-              <TableCell>3</TableCell>
-              <TableCell>4</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>0</TableCell>
-              <TableCell>1</TableCell>
-              <TableCell>2</TableCell>
-              <TableCell>3</TableCell>
-              <TableCell>4</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>0</TableCell>
-              <TableCell>1</TableCell>
-              <TableCell>2</TableCell>
-              <TableCell>3</TableCell>
-              <TableCell>4</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>0</TableCell>
-              <TableCell>1</TableCell>
-              <TableCell>2</TableCell>
-              <TableCell>3</TableCell>
-              <TableCell>4</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>0</TableCell>
-              <TableCell>1</TableCell>
-              <TableCell>2</TableCell>
-              <TableCell>3</TableCell>
-              <TableCell>4</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>0</TableCell>
-              <TableCell>1</TableCell>
-              <TableCell>
-                <TableRow>
-                  <TableCell>0</TableCell>
-                  <TableCell>1</TableCell>
-                  <TableCell>2</TableCell>
-                  <TableCell>3</TableCell>
-                  <TableCell>4</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>0</TableCell>
-                  <TableCell>1</TableCell>
-                  <TableCell>2</TableCell>
-                  <TableCell>3</TableCell>
-                  <TableCell>4</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>0</TableCell>
-                  <TableCell>1</TableCell>
-                  <TableCell>2</TableCell>
-                  <TableCell>3</TableCell>
-                  <TableCell>4</TableCell>
-                </TableRow>
-              </TableCell>
-              <TableCell>3</TableCell>
-              <TableCell>4</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>0</TableCell>
-              <TableCell>1</TableCell>
-              <TableCell>2</TableCell>
-              <TableCell>3</TableCell>
-              <TableCell>4</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>0</TableCell>
-              <TableCell>1</TableCell>
-              <TableCell>2</TableCell>
-              <TableCell>3</TableCell>
-              <TableCell>4</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>0</TableCell>
-              <TableCell>1</TableCell>
-              <TableCell>2</TableCell>
-              <TableCell>3</TableCell>
-              <TableCell>4</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>0</TableCell>
-              <TableCell>1</TableCell>
-              <TableCell>2</TableCell>
-              <TableCell>3</TableCell>
-              <TableCell>4</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>0</TableCell>
-              <TableCell>1</TableCell>
-              <TableCell>2</TableCell>
-              <TableCell>3</TableCell>
-              <TableCell>4</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>0</TableCell>
-              <TableCell>1</TableCell>
-              <TableCell>2</TableCell>
-              <TableCell>3</TableCell>
-              <TableCell>4</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>0</TableCell>
-              <TableCell>1</TableCell>
-              <TableCell>2</TableCell>
-              <TableCell>3</TableCell>
-              <TableCell>4</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>0</TableCell>
-              <TableCell>1</TableCell>
-              <TableCell>2</TableCell>
-              <TableCell>3</TableCell>
-              <TableCell>4</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination></TablePagination>
-    </>
-  );
 }
-
-export default StickyHeadTable;
