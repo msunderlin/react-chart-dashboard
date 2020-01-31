820cc9503c64442d1f9a52646f05c232ffc04ea1
Michael Sunderlin <msunderlin@gmail.com>
2020-01-31 09:12:40 -0800

    cleaning up the chartjs a bit

--- public/assets/js/getCharts.js
+++ public/assets/js/getCharts.js
@@ -4,11 +4,10 @@ function getCharts() {
   data.push({
     type: "bar",
     title: "Daily PL 01/05/2020",
-    stacked: 1,
     interval: 20000,
     defaultpos: { h: 2, w: 2, minW: 2, minH: 2 },
     source:
-      "http://local.admin.admediary.com/test/getArray.php?report=dailyplstacked&date=01/05/2020"
+      "http://local.admin.admediary.com/test/getArray.php?report=dailypl&date=01/05/2020"
   });
   data.push({
     type: "pie",
@@ -21,19 +20,18 @@ function getCharts() {
   data.push({
     type: "bar",
     title: "Daily PL 01/07/2020",
-    stacked: 1,
     interval: 20000,
     defaultpos: { h: 2, w: 2, minW: 2, minH: 2 },
     source:
-      "http://local.admin.admediary.com/test/getArray.php?report=dailyplstacked&date=01/07/2020"
+      "http://local.admin.admediary.com/test/getArray.php?report=dailypl&date=01/07/2020"
   });
   data.push({
-    type: "bar",
+    type: "pie",
     title: "Daily PL 01/07/2020",
     interval: 20000,
     defaultpos: { h: 2, w: 5, minW: 2, minH: 2 },
     source:
-      "http://local.admin.admediary.com/test/getArray.php?report=dailypl&date=01/07/2020"
+    "http://local.admin.admediary.com/test/getArray.php?report=dailyplpie&date=01/07/2020"
   });
   data.push({
     type: "table",
@@ -56,6 +54,28 @@ function getCharts() {
     source:
       "http://local.admin.admediary.com/test/getArray.php?report=dailyplreport&date=01/05/2020"
   });
+  data.push({
+    type: "table",
+    interval: 10000,
+    columns: [
+      {title:"Type",field: "affiliate_id",headerStyle: {minWidth: 50}, cellStyle: {minWidth: 50} },
+      {title:"Affiliate ID",field: "sub_id",type:"numeric",headerStyle: {minWidth: 50}, cellStyle: {minWidth: 50} },
+      {title:"Affiliate Name",field: "affiliate_name"},
+      {title:"Count",field: "count",type:"numeric",headerStyle: {minWidth: 50}, cellStyle: {minWidth: 50} },
+      {title:"Earned (4)",field: "earned",type:"numeric",headerStyle: {minWidth: 50}, cellStyle: {minWidth: 50} },
+      {title:"Cost ($)",field: "cost",type:"numeric",headerStyle: {minWidth: 50}, cellStyle: {minWidth: 50} },
+      {title:"Profit ($)",field: "profit",type:"numeric",headerStyle: {minWidth: 50}, cellStyle: {minWidth: 50} },
+      {title:"Margin (%)",field: "margin",type:"numeric",headerStyle: {minWidth: 50}, cellStyle: {minWidth: 50} },
+      {title:"Sold",field: "sold",type:"numeric",headerStyle: {minWidth: 50}, cellStyle: {minWidth: 50} },
+      {title:"Unsold",field: "unsold",type:"numeric",headerStyle: {minWidth: 50}, cellStyle: {minWidth: 50} },
+      {title:"Paid",field: "paid",type:"numeric",headerStyle: {minWidth: 50}, cellStyle: {minWidth: 50} },
+    ],
+    title: "Daily PL 01/07/2020",
+    defaultpos: { h: 5, w: 6, minW: 6, minH: 5 },
+    source:
+      "http://local.admin.admediary.com/test/getArray.php?report=dailyplreport&date=01/07/2020"
+  });
+
 
   return data;
 }
