ebd0feef2902da8975022641d7b5cf89574c1a37
Michael Sunderlin <msunderlin@gmail.com>
2020-04-13 08:20:09 -0700

    removing swapfiles from git commits.

--- src/components/charts/ChartWrapper.js
+++ src/components/charts/ChartWrapper.js
@@ -7,13 +7,15 @@ import PolarChart from "./PolarChart";
 import StackedBarChart from "./StackedBarChart";
 import StackedLineChart from "./StackedLineChart";
 import Table from "../table/Table";
+
 class ChartWrapper extends Component {
   constructor(props) {
     super(props);
     this.state = {
 	    widget_id: this.props.widget_id,
 	widget: [],
-	data: []
+	data: [],
+	
     };
 
     let url = window.base_url+this.props.chart.source;
@@ -24,8 +26,11 @@ class ChartWrapper extends Component {
       .then(data => {
         this.setState({ feeds: data });
       });
+    
     this.getHeader = null;
   }
+
+  //storing the instance of the set interval here.  
   timer = 0;
 
   componentDidMount() {
@@ -45,13 +50,69 @@ class ChartWrapper extends Component {
     }
   }
 async getWidget(id){
-	let widget = null;
 	const action = "get_widget";
 	const user_id = window.getUserID();
-	const dashboard_id = window.getDashboardId();
-	const url = window.ajax_url +"?user_id="+
-
+	const url = window.ajax_url +"?user_id="+user_id+"&widget_id="+id;
+	const widget = await fetch(url)
+		.then(response =>response.json())
+		.then(data => {
+			return data;
+})
+	.catch(error =>{
+		console.error("Error:", error);
+		return false;
+	});
+	return await widget;
 }
+
+async getSource(id){
+	const action = "get_source";
+	const user_id = window.getUserID();
+	const url = window.ajax_url +"?user_id="+user_id+"&source_id="+id;
+	const source = await fetch(url)
+		.then(response =>response.json())
+		.then(data => {
+			return data.source;
+	})
+	.catch(error =>{
+		console.error("Error:", error);
+		return false;
+	});
+	return await source;
+}
+	async getSources(){
+		const action = "get_sources";
+		const user_id = window.getUserID();
+		const url = window.ajax_url +"?user_id="+user_id;
+		const sources = await fetch(url)
+			.then(response => response.json())
+			.then(data => {
+				return data.sources;
+			})
+			.catch(error => {
+				console.error("Error:", error);
+			return false;
+			});
+			
+		return await sources;
+	}	
+	async saveToDb(){
+		const action = "save_widget";
+		const user_id = window.getUserID();
+		const url = window.ajax_url +"?user_id="+user_id;
+		const sources = await fetch(url)
+			.then(response => response.json())
+			.then(data => {
+				return data.sources;
+			})
+			.catch(error => {
+				console.error("Error:", error);
+			return false;
+			});
+			
+		return await sources;
+
+	}
   componentWillUnmount() {
     clearInterval(this.timer);
     this.setState({feeds:[]});
@@ -172,6 +233,79 @@ async getWidget(id){
         return "";
     }
  }
+// State Update Methods
+  handleTitleChange = event => {
+    let edit_target = { ...this.state.edit_target };
+    edit_target.title = event.target.value;
+    this.setState(state => ({
+      edit_target
+    }));
+  };
+
+  handleTypeChange = event => {
+    let edit_target = { ...this.state.edit_target };
+    edit_target.type = event.target.value;
+    this.setState(state => ({
+      edit_target
+    }));
+  };
+
+  handleStartChange = date => {
+    let edit_target = { ...this.state.edit_target };
+    edit_target.params.start_date = date.format("l");
+    this.setState(state => ({
+      edit_target
+    }));
+  };
+  handleEndChange = date => {
+    let edit_target = { ...this.state.edit_target };
+    edit_target.params.end_date = date.format("l");
+    this.setState(state => ({
+      edit_target
+    }));
+  };
+  handleParamDataTypeChange = event => {
+    let edit_target = { ...this.state.edit_target };
+    edit_target.params.datatype = event.target.value;
+    this.setState(state => ({
+      edit_target
+    }));
+  };
+  handleProductChange = event => {
+    let edit_target = { ...this.state.edit_target };
+    edit_target.params.product_id = event.target.value;
+    this.setState(state => ({
+      edit_target
+    }));
+  };
+  handleParamIntervalChange = event => {
+    let edit_target = { ...this.state.edit_target };
+    edit_target.params.interval = event.target.value;
+    this.setState(state => ({
+      edit_target
+    }));
+  };
+
+  handleIntervalChange = event => {
+    let edit_target = { ...this.state.edit_target };
+    edit_target.interval = event.target.value;
+    this.setState(state => ({
+      edit_target
+    }));
+  };
+  handleChartChange = () => {
+    if (this.state.chart.length > 0) {
+      console.log();
+      let chart = this.state.chart;
+      chart[this.state.edit_target_id] = this.state.edit_target;
+      this.setState(state => ({
+        chart
+      }));
+      this.saveChartsToDB(this.state.layouts, chart);
+    }
+  };
+
+
 }
 
 export default ChartWrapper;
